551945335db907d33cfce1f918b26b432762a00c
Keerthi-mavi <88768410+Keerthi-mavi@users.noreply.github.com>
2025-07-14 09:59:55 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
                         <h2 className='text-3xl font-bold'>Mavillapally Keerthi</h2>
                         <h3 className='text-2xl font-semibold mb-9'>Full-Stack developer</h3>
                         <p class="text-lg mb-6 ">
-                            Software Engineer with more than 3 years of experience as a Fullstack developer. Experienced in Web Development, adept in contributing through all stages of web development. Good communication skills and a team player with a strong willingness to learn new technologies.
+                            Software Engineer with 4 years of experience as a Fullstack developer. Experienced in Web Development, adept in contributing through all stages of web development. Good communication skills and a team player with a strong willingness to learn new technologies.
                         </p>
                         <p class="text-lg mb-6 ">
                             Interested in exploring new technologies, integrating and playing around with open source softwares.
